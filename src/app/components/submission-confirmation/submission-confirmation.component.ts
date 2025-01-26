617a52475dac9cb3501dd1d2308d7fe036f06342
Macka <martina.bobokova@gmail.com>
2025-01-26 17:01:52 +0100

    created app structure

---
+++ src/app/components/submission-confirmation/submission-confirmation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-submission-confirmation',
+  standalone: true,
+  imports: [],
+  templateUrl: './submission-confirmation.component.html',
+  styleUrl: './submission-confirmation.component.css'
+})
+export class SubmissionConfirmationComponent {
+
+}
