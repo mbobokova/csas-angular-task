e8c35568ce701d73908a00fdb35f2ac4605b4f04
Macka <martina.bobokova@gmail.com>
2025-01-26 23:12:11 +0100

    create server.ts for formData Api

--- server.ts
+++ server.ts
@@ -1,6 +1,11 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { CommonEngine } from '@angular/ssr';
 import express from 'express';
+// imports for API post '/api/submit-form'
+import * as fs from 'fs';
+import * as path from 'path';
+import * as cors from 'cors';
+
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
@@ -38,6 +43,22 @@ export function app(): express.Express {
       })
       .then((html) => res.send(html))
       .catch((err) => next(err));
+  });
+  
+  // Server Post for formData from validation.component
+  server.post('/api/submit-form', (req, res) => {
+    const formData = req.body;
+    const jsonData = JSON.stringify(formData, null, 2);
+    const filePath = path.join(__dirname, 'form-data.json');
+  
+    fs.writeFile(filePath, jsonData, (err) => {
+      if (err) {
+        console.error('Error writing file', err);
+        res.status(500).send('Error saving data');
+      } else {
+        res.status(200).send('Data saved successfully');
+      }
+    });
   });
 
   return server;
